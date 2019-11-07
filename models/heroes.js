90f2a37171e4a24e156cf91f9735dcc67804630b
ashishpokhrel123 <aashishpokhrel146@gmail.com>
2019-11-07 18:53:34 +0545

    adding routes

---
+++ models/heroes.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const commentSchema = new mongoose.Schema({
+    comment:{
+        type: String,
+        required: true
+    }
+},{ timestamps: true});
+const heroesSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    desc:{
+        type: String,
+        required: true
+    },
+    comment: [commentSchema]
+},{timestamps:true});
+
+module.exports = mongoose.model('heroes', heroesSchema);
+
